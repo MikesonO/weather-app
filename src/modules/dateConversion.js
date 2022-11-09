0fdaa2fc61fecdf1f07c496908255bed251566f5
mikeson <mikeson.jnr@gmail.com>
2022-11-09 23:26:01 +0000

    Update getLocalDate function to return Day of Week

--- src/modules/dateConversion.js
+++ src/modules/dateConversion.js
@@ -7,10 +7,12 @@ export default function getLocalDate(timezone) {
   const time = date.getTime();
   const localTime = time + 1000 * timezone;
   let localDate = new Date(localTime);
+  const getDay = format(localDate, "EEEE");
   const getTime = format(localDate, "p");
   localDate = format(localDate, "do MMMM yyyy");
 
   return {
+    day: getDay,
     date: localDate,
     time: getTime
   }
