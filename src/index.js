55dd7624045616f509c6492d440a9f478a509c6a
mikeson <mikeson.jnr@gmail.com>
2022-11-10 23:24:54 +0000

    Set up Font Awesome

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import weatherApi from './modules/weatherApi';
 import './styles/styles.scss';
 
+// Font Awesome
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 weatherApi();
